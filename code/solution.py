d7988b4db1fbbc4155c334f4cc9ce3001fb8ffe2
Yan Zhang <yanzh22@outlook.com>
2018-11-27 14:34:26 +0100

    Finish the task 3.

--- code/solution.py
+++ code/solution.py
@@ -12,11 +12,10 @@ chs = pd.read_stata('d:/Eriko-/prog-econ-sandbox/assignment-3-group_7/bld/chs_da
 childids = chs.childid.unique().tolist()
 
 # Discard observations in which year is not in range(1986, 2011, 2)
-year = list(range(1986, 2011, 2))
+selected_years = list(range(1986, 2011, 2))
 
-# Define the is_in function as the filer condition
-def is_in(year):
+a = chs.year.isin(selected_years)
 
-    
-ids_year = list(filter(is_in, childids))
+
+
 
